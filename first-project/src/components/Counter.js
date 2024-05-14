84368f5d4dcb8c176d1a794be3dcc45b997f5422
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-14 20:43:25 +0300

    useState hook'unu kullanarak dinamik bir variable olusturup componente fonksiyonelite ekledik

--- first-project/src/components/Counter.js
+++ first-project/src/components/Counter.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./Counter.css";
 
 const Counter = () => {
+  //virtual DOM'un counter degerinin degistigini anlayabilmesi icin useState hooku kullandik
+  const [counter, setCounter] = useState(0);
+
+  function arttir() {
+    setCounter(counter + 1);
+  }
+
+  function azalt() {
+    setCounter(counter - 1);
+  }
   return (
     <>
-      <h2>Counter: 0</h2>
+      <h2>Counter: {counter}</h2>
       <div>
-        <button>+1</button>
-        <button>-1</button>
+        <button onClick={arttir}>+1</button>
+        <button onClick={azalt}>-1</button>
       </div>
     </>
   );
