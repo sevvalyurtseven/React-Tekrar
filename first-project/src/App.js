1b63b913f2e69dc7fc5bc95e3623880bbeb8d1ba
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-14 20:26:30 +0300

    header componentini root componente bagladik

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return <Header />;
 }
 
 export default App;
