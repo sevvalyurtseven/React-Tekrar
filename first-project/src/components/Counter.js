cdef598201d3575159db116bdabac859953322a5
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-14 20:34:16 +0300

    text-align: center

--- first-project/src/components/Counter.js
+++ first-project/src/components/Counter.js
@@ -1,6 +1,14 @@
 import "./Counter.css";
 
 const Counter = () => {
-  return <></>;
+  return (
+    <>
+      <h2>Counter: 0</h2>
+      <div>
+        <button>+1</button>
+        <button>-1</button>
+      </div>
+    </>
+  );
 };
 export default Counter;
