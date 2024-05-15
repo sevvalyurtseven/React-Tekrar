39459a18e895c96eecb77bd3e1741890533e38c8
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-15 12:26:00 +0300

    ortak fonksiyonlar için utils klasörü olusturmak ve kullanım örneği

--- first-project/src/components/Header.js
+++ first-project/src/components/Header.js
@@ -1,10 +1,18 @@
 import "./Header.css";
+import { getFullName } from "../utils/utils";
 
+export const user = {
+  name: "Sevval",
+  surname: "Yurtseven",
+  age: 24,
+};
 function Header() {
   const projectName = "My First Project";
   return (
     <header>
-      <h1>Welcome to {projectName}</h1>
+      <h1>
+        Welcome {getFullName(user)} to {projectName}
+      </h1>
     </header>
   );
 }
---
+++ first-project/src/utils/utils.js
@@ -0,0 +1,3 @@
+export function getFullName(user) {
+  return `${user.name} ${user.surname}`;
+}
