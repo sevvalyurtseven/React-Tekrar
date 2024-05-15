3f60034bdae07b0a0ac465b8fe06d936714302e8
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-15 20:56:51 +0300

    user card olusturduk

---
+++ first-project/src/components/user/User.js
@@ -0,0 +1,11 @@
+function User(props) {
+  const { user } = props;
+  return (
+    <div className="user-card">
+      <h2>{user.first_name}</h2>
+      <p>{user.email}</p>
+      <img src={user.avatar} alt={user.first_name} />
+    </div>
+  );
+}
+export default User;
