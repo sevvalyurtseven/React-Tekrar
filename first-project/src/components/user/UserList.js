f2da99d26de75e433538dd069104ec9c61f5f953
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-15 20:57:35 +0300

    her bir user'i mapledik

---
+++ first-project/src/components/user/UserList.js
@@ -0,0 +1,13 @@
+import User from "./User";
+
+function UserList(props) {
+  const { users } = props;
+  return (
+    <div className="user-container">
+      {users.map((user) => (
+        <User user={user} />
+      ))}
+    </div>
+  );
+}
+export default UserList;
