1f35ee1f8eb909f35dec11fff5801f4e719d0bea
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-16 19:43:29 +0300

    create not found page

--- first-project/src/layout/Main.js
+++ first-project/src/layout/Main.js
@@ -1,6 +1,7 @@
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom";
 import Counter from "../components/Counter";
 import Users from "../pages/Users";
+import NotFound from "../pages/NotFound";
 
 function Main(props) {
   const { name } = props;
@@ -12,6 +13,9 @@ function Main(props) {
         </Route>
         <Route path="/users">
           <Users />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </div>
