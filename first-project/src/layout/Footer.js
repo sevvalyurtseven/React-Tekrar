678e66a3c4c5c48d69c8ae42fa445f875c5c8705
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-16 18:56:58 +0300

    footer and sidebar template

---
+++ first-project/src/layout/Footer.js
@@ -0,0 +1,8 @@
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright @2024</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ first-project/src/layout/SideBar.js
@@ -0,0 +1,10 @@
+function SideBar(props) {
+  return (
+    <div className="side-container">
+      <a href="#">link 1</a>
+      <a href="#">link 2</a>
+      <a href="#">link 3</a>
+    </div>
+  );
+}
+export default SideBar;
