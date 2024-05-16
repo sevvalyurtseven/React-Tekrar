3c40d4b673e963293a7d26538ca97ad6950cc0a9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-16 20:16:09 +0300

    useHistory hooku ile sayfa yönlendirmesi yaptık

--- first-project/src/pages/User.js
+++ first-project/src/pages/User.js
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import { useParams, useHistory } from "react-router-dom";
+import { Button, Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import axios from "axios";
 
 const User = () => {
   const { id } = useParams();
 
   const [user, setUser] = useState({});
+
+  const history = useHistory();
+
+  function handlePrev() {
+    history.goBack();
+  }
+
+  function handleNavigation() {
+    history.push("/counter");
+  }
 
   useEffect(() => {
     axios
@@ -23,15 +33,21 @@ const User = () => {
   }, []);
 
   return (
-    <Card className="shadow-sm my-2" style={{ width: "18rem" }}>
-      <img src={user.avatar} alt={user.first_name} />
-      <CardBody>
-        <CardTitle tag={"h5"}>{user.first_name}</CardTitle>
-        <CardSubtitle className="mb-2 text-muted" tag={"h6"}>
-          {user.email}
-        </CardSubtitle>
-      </CardBody>
-    </Card>
+    <>
+      <Button className="me-4" onClick={handlePrev}>
+        Geri
+      </Button>
+      <Button onClick={handleNavigation}>Counter Page</Button>
+      <Card className="shadow-sm my-2" style={{ width: "18rem" }}>
+        <img src={user.avatar} alt={user.first_name} />
+        <CardBody>
+          <CardTitle tag={"h5"}>{user.first_name}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag={"h6"}>
+            {user.email}
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+    </>
   );
 };
 export default User;
