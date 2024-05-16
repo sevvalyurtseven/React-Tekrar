2113f46fa82b6992203e42304d02f2562ac61910
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-16 12:37:35 +0300

    button reactstrap butonu ile degişti

--- first-project/src/components/CounterController.js
+++ first-project/src/components/CounterController.js
@@ -1,13 +1,23 @@
+import { Button } from "reactstrap";
+
 function CounterController(props) {
   const { counter, arttir, azalt } = props;
   return (
     <div>
-      <button disabled={counter >= 10 ? true : false} onClick={arttir}>
-        +1
-      </button>
-      <button disabled={counter <= 0 ? true : false} onClick={azalt}>
-        -1
-      </button>
+      <Button
+        color="success"
+        disabled={counter >= 10 ? true : false}
+        onClick={arttir}
+      >
+        Arttır
+      </Button>
+      <Button
+        color="danger"
+        disabled={counter <= 0 ? true : false}
+        onClick={azalt}
+      >
+        Azalt
+      </Button>
     </div>
   );
 }
