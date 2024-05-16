e29f33f7aeabfee08855f60fa17751950ac07fe9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-16 23:46:44 +0300

    conditional rendering

--- first-project/src/components/user/User.js
+++ first-project/src/components/user/User.js
@@ -2,17 +2,23 @@ import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import "./User.css";
 
 function User(props) {
-  const { user } = props;
-  return (
-    <Card className="shadow-sm my-2" style={{ width: "18rem" }}>
-      <img src={user.avatar} alt={user.first_name} />
-      <CardBody>
-        <CardTitle tag={"h5"}>{user.first_name}</CardTitle>
-        <CardSubtitle className="mb-2 text-muted" tag={"h6"}>
-          {user.email}
-        </CardSubtitle>
-      </CardBody>
-    </Card>
-  );
+  const { user, type } = props;
+  if (type === "listMode") {
+    return (
+      <CardTitle tag={"h5"}>{user.first_name + " " + user.last_name}</CardTitle>
+    );
+  } else {
+    return (
+      <Card className="shadow-sm my-2" style={{ width: "18rem" }}>
+        <img src={user.avatar} alt={user.first_name} />
+        <CardBody>
+          <CardTitle tag={"h5"}>{user.first_name}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag={"h6"}>
+            {user.email}
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+    );
+  }
 }
 export default User;
