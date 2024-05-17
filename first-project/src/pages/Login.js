592280dfe927f346f02a248798929735d5cf0eb1
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-17 13:14:55 +0300

    add checkbox and radio button

--- first-project/src/pages/Login.js
+++ first-project/src/pages/Login.js
@@ -6,6 +6,7 @@ function Login() {
           E-Mail:
           <input
             id="email"
+            name="email"
             type="email"
             placeholder="E-Mail adresinizi giriniz"
           />
@@ -16,10 +17,28 @@ function Login() {
           Password:
           <input
             id="password"
+            name="password"
             type="password"
             placeholder="Şifrenizi giriniz"
           />
         </label>
+      </div>
+      <p>Which position are you working on?</p>
+      <div>
+        <input id="hr" name="position" type="radio" value="hr" />
+        <label htmlFor="hr">HR</label>
+      </div>
+      <div>
+        <input id="sales" name="position" type="radio" value="sales" />
+        <label htmlFor="sales">Sales & Marketing</label>
+      </div>
+      <div>
+        <input id="education" name="position" type="radio" value="education" />
+        <label htmlFor="education">Education</label>
+      </div>
+      <div>
+        <input id="agreement" name="agreement" type="checkbox" />
+        <label htmlFor="agreement">I accept the terms and conditions.</label>
       </div>
       <button type="submit">Giriş Yap</button>
     </form>
